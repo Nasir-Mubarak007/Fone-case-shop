50c173887b8deecc8d34d6a8a8595d3658f5bcb6
Nasir-Mubarak007 <nmubarak717@gmail.com>
2024-10-21 10:06:09 +0100

    1st vercel deployment ever

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { getPaymentStatus } from './actions'
 
-const ThankYou = () => {
+const ThankYou = ({orderId}:{orderId: string}) => {
     const {} = useQuery({
         queryKey: ['get-payment-status'],
         queryFn: async()=> await getPaymentStatus()
